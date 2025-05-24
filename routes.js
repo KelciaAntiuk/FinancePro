fe0d4df7e48a27fde0b6ea1e4f168baed54fb396
Julian Alves <julianalvesinstinto@gmail.com>
2025-05-24 19:09:58 -0300

    feat: added ID

--- routes.js
+++ routes.js
@@ -6,9 +6,12 @@ const financeiro = [
     {
         descricao: "Bala",
         valor: 1.00,
-        tipo: 2
+        tipo: 2,
+        id: 1
     }
 ];
+
+let ultimoId = 1;
 
 route.get('/', (req, res) => {
     res.render('index'); 
@@ -20,11 +23,13 @@ route.post('/criar', (req, res)=>{
     const desc = req.body.descricao;
     const valor = req.body.valor;
     const tipo = req.body.tipo
+    ultimoId++;
 
     financeiro.push({
         desc,
         valor: parseFloat(valor),
-        tipo: parseInt(tipo)
+        tipo: parseInt(tipo),
+        id: ultimoId
     });
 
     res.redirect('/');
